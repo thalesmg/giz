7ab6042e045d55081e726046036ddf2cd620406a
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-03-08 21:51:15 -0300

    gênese

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "gist manager";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix-src.url = "github:nix-community/poetry2nix";
+  };
+
+  outputs = { self, nixpkgs, poetry2nix-src, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            poetry2nix-src.overlay
+          ];
+        };
+      in
+        {
+          # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+          # defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+
+          defaultPackage = pkgs.poetry2nix.mkPoetryApplication {
+            projectDir = self;
+          };
+
+          devShell = pkgs.mkShell {
+            buildInputs = [
+              pkgs.python39Packages.PyGithub
+            ];
+          };
+        }
+    );
+}
